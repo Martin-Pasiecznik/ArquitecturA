62d3725442bf15e8cd5e94695f504dabe711c94b
Martin Pasiecznik <104079011+Martin-Pasiecznik@users.noreply.github.com>
2024-12-31 09:40:02 -0300

    update 2025

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 {
+    ///// Configuration of diseños.html modal ///////
+
     const zoomBtn = document.querySelectorAll('.img_diseño');
     const imageView = document.querySelector('.image-view');
     const nextBtn = document.getElementById('next-btn');
@@ -181,20 +183,20 @@
     })
 
     function currentImageDisplay(position) {
-        imageBox4.style.background = `url(imagenes/c${currentImageIdx4}.jpeg) center/cover no-repeat`;
+        imageBox4.style.background = `url(imagenes/img2025/Foto${currentImageIdx4}.jpg) center/cover no-repeat`;
     }
 
     prevBtn4.addEventListener('click', function () {
         currentImageIdx4--;
         if (currentImageIdx4 === 0) {
-            currentImageIdx4 = 6;
+            currentImageIdx4 = 8;
         }
         currentImageDisplay(currentImageIdx4);
     })
 
     nextBtn4.addEventListener('click', function () {
         currentImageIdx4++;
-        if (currentImageIdx4 === 7) {
+        if (currentImageIdx4 === 9) {
             currentImageIdx4 = 1;
         }
         currentImageDisplay(currentImageIdx4);
@@ -202,6 +204,149 @@
 
 }
 
+{
+
+    const zoomBtn5 = document.querySelectorAll('.img_diseño5');
+    const imageView = document.querySelector('.image-view');
+    const nextBtn5 = document.getElementById('next-btn5');
+    const prevBtn5 = document.getElementById('prev-btn5');
+    const imageBox5 = document.querySelector('.image-box5');
+
+
+    let currentImageIdx5 = 0;
+
+    imageView.addEventListener('click', function () {
+        this.style.display = "none";
+        imageBox5.style.display = "none";
+    })
+
+
+    zoomBtn5.forEach(function (btn, index5) {
+        btn.addEventListener('click', function () {
+            imageView.style.display = "block";
+            imageBox5.style.display = "block";
+            currentImageIdx5 = index5 + 1;
+            currentImageDisplay(currentImageIdx5);
+        })
+    })
+
+    function currentImageDisplay(position) {
+        imageBox5.style.background = `url(imagenes/c${currentImageIdx5}.jpeg) center/cover no-repeat`;
+    }
+
+    prevBtn5.addEventListener('click', function () {
+        currentImageIdx5--;
+        if (currentImageIdx5 === 0) {
+            currentImageIdx5 = 6;
+        }
+        currentImageDisplay(currentImageIdx5);
+    })
+
+    nextBtn5.addEventListener('click', function () {
+        currentImageIdx5++;
+        if (currentImageIdx5 === 7) {
+            currentImageIdx5 = 1;
+        }
+        currentImageDisplay(currentImageIdx5);
+    })
+
+}
+
+{
+
+    const zoomBtn6 = document.querySelectorAll('.img_diseño6');
+    const imageView = document.querySelector('.image-view');
+    const nextBtn6 = document.getElementById('next-btn6');
+    const prevBtn6 = document.getElementById('prev-btn6');
+    const imageBox6 = document.querySelector('.image-box6');
+
+
+    let currentImageIdx6 = 0;
+
+    imageView.addEventListener('click', function () {
+        this.style.display = "none";
+        imageBox6.style.display = "none";
+    })
+
+
+    zoomBtn6.forEach(function (btn, index6) {
+        btn.addEventListener('click', function () {
+            imageView.style.display = "block";
+            imageBox6.style.display = "block";
+            currentImageIdx6 = index6 + 1;
+            currentImageDisplay(currentImageIdx6);
+        })
+    })
+
+    function currentImageDisplay(position) {
+        imageBox6.style.background = `url(imagenes/img2025/p${currentImageIdx6}.jpg) center/cover no-repeat`;
+    }
+
+    prevBtn6.addEventListener('click', function () {
+        currentImageIdx6--;
+        if (currentImageIdx6 === 0) {
+            currentImageIdx6 = 4;
+        }
+        currentImageDisplay(currentImageIdx6);
+    })
+
+    nextBtn6.addEventListener('click', function () {
+        currentImageIdx6++;
+        if (currentImageIdx6 === 5) {
+            currentImageIdx6 = 1;
+        }
+        currentImageDisplay(currentImageIdx6);
+    })
+
+}
+
+{
+
+    const zoomBtn7 = document.querySelectorAll('.img_diseño7');
+    const imageView = document.querySelector('.image-view');
+    const nextBtn7 = document.getElementById('next-btn7');
+    const prevBtn7 = document.getElementById('prev-btn7');
+    const imageBox7 = document.querySelector('.image-box7');
+
+
+    let currentImageIdx7 = 0;
+
+    imageView.addEventListener('click', function () {
+        this.style.display = "none";
+        imageBox7.style.display = "none";
+    })
+
+
+    zoomBtn7.forEach(function (btn, index7) {
+        btn.addEventListener('click', function () {
+            imageView.style.display = "block";
+            imageBox7.style.display = "block";
+            currentImageIdx7 = index7 + 1;
+            currentImageDisplay(currentImageIdx7);
+        })
+    })
+
+    function currentImageDisplay(position) {
+        imageBox7.style.background = `url(imagenes/img2025/a${currentImageIdx7}.jpg) center/cover no-repeat`;
+    }
+
+    prevBtn7.addEventListener('click', function () {
+        currentImageIdx7--;
+        if (currentImageIdx7 === 0) {
+            currentImageIdx7 = 5;
+        }
+        currentImageDisplay(currentImageIdx7);
+    })
+
+    nextBtn7.addEventListener('click', function () {
+        currentImageIdx7++;
+        if (currentImageIdx7 === 6) {
+            currentImageIdx7 = 1;
+        }
+        currentImageDisplay(currentImageIdx7);
+    })
+
+}
 
 //////////////////////////////////////////////////
 // CONSTRUCCIONES MODAL //
